43a2336d6cef9d61aae23ac2e59d5ca888e46e7d
Rich Holman <richholman@Richs-MacBook-Pro-6.local>
2022-11-29 17:20:10 +0000

    Overhaul focus styles

--- inc/dgwltd-acf.php
+++ inc/dgwltd-acf.php
@@ -15,3 +15,19 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		)
 	);
 }
+
+
+if ( ! function_exists( 'rnid_acf_color_palette' ) ) :
+function rnid_acf_color_palette() {
+?>
+	<script type="text/javascript">
+	(function($) {
+		acf.add_filter('color_picker_args', function( args, $field ){
+			args.palettes = ['#0095a3', '#fe9f97', '#fbae17', '#0d0f05', '#f1f2f0', '#e0e5db']
+			return args;
+		});
+	})(jQuery);
+	</script>
+<?php }
+add_action('acf/input/admin_footer', 'rnid_acf_color_palette');
+endif;
