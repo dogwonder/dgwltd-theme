12a0d66781d6fd0e9adda1b6a844b287236c6bac
Rich Holman <richholman@Richs-MacBook-Pro-8.local>
2023-01-12 16:22:12 +0000

    Update social icons to Font Awesome latest

--- template-parts/_molecules/social-links.php
+++ template-parts/_molecules/social-links.php
@@ -1,12 +1,13 @@
 <?php 
 $social = get_field('social_links', 'options');
 $facebook = $social['facebook_link'];
-$twitter = $social['twitter_link'];
-$instagram = $social['instagram_link'];
-$tiktok = $social['tiktok_link'];
-$youtube = $social['youtube_link'];
-$linkedin = $social['linkedin_link'];
 $github = $social['github_link'];
+$instagram = $social['instagram_link'];
+$linkedin = $social['linkedin_link'];
+$mastodon = $social['mastodon_link'];
+$tiktok = $social['tiktok_link'];
+$twitter = $social['twitter_link'];
+$youtube = $social['youtube_link'];
 ?>
 <ul class="dgwltd-footer__social-links dgwltd-footer__inline-list">
 
@@ -15,56 +16,6 @@ $github = $social['github_link'];
         <a href="<?php echo ($facebook ? $facebook : 'https://www.facebook.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our Facebook page">
             <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__facebook">
             <use xlink:href="#social-facebook" />
-            </svg>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if($twitter) : ?>    
-    <li>
-        <a href="<?php echo ($twitter ? $twitter : 'https://twitter.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our Twitter page">
-            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__twitter">
-            <use xlink:href="#social-twitter" />
-            </svg>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if($instagram) : ?>    
-    <li>
-        <a href="<?php echo ($instagram ? $instagram : 'https://instagram.com/'); ?>" rel="external noopener noreferrer" aria-label="Follow us on Instagram">
-            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__instagram">
-            <use xlink:href="#social-instagram" />
-            </svg>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if($tiktok) : ?>
-    <li>
-        <a href="<?php echo ($tiktok ? $tiktok : 'https://www.tiktok.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our TikTok profile">
-            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__tiktok">
-            <use xlink:href="#social-tiktok" />
-            </svg>
-        </a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if($youtube) : ?>
-    <li>
-        <a href="<?php echo ($youtube ? $youtube : 'https://www.youtube.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our YouTube channel">
-            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__youtube">
-            <use xlink:href="#social-youtube" />
-            </svg>
-        </a>
-    </li>
-    <?php endif; ?>
-
-    <?php if($linkedin) : ?>
-    <li>
-        <a href="<?php echo ($linkedin ? $linkedin : 'https://www.linkedin.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our Linkedin profile">
-            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__linkedin">
-            <use xlink:href="#social-linkedin" />
             </svg>
         </a>
     </li>
@@ -80,5 +31,64 @@ $github = $social['github_link'];
     </li>
     <?php endif; ?>
 
+    <?php if($instagram) : ?>    
+    <li>
+        <a href="<?php echo ($instagram ? $instagram : 'https://instagram.com/'); ?>" rel="external noopener noreferrer" aria-label="Follow us on Instagram">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__instagram">
+            <use xlink:href="#social-instagram" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
+
+    <?php if($linkedin) : ?>
+    <li>
+        <a href="<?php echo ($linkedin ? $linkedin : 'https://www.linkedin.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our Linkedin profile">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__linkedin">
+            <use xlink:href="#social-linkedin" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
+
+    <?php if($mastodon) : ?>
+    <li>
+        <a href="<?php echo $mastodon; ?>" rel="external noopener noreferrer" aria-label="Visit our Mastodon profile">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__mastodon">
+            <use xlink:href="#social-mastodon" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
     
+    <?php if($tiktok) : ?>
+    <li>
+        <a href="<?php echo ($tiktok ? $tiktok : 'https://www.tiktok.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our TikTok profile">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__tiktok">
+            <use xlink:href="#social-tiktok" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
+
+    <?php if($twitter) : ?>    
+    <li>
+        <a href="<?php echo ($twitter ? $twitter : 'https://twitter.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our Twitter page">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__twitter">
+            <use xlink:href="#social-twitter" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
+
+    <?php if($youtube) : ?>
+    <li>
+        <a href="<?php echo ($youtube ? $youtube : 'https://www.youtube.com/'); ?>" rel="external noopener noreferrer" aria-label="Visit our YouTube channel">
+            <svg aria-hidden="true" focusable="false" class="icon icon-social icon-social__youtube">
+            <use xlink:href="#social-youtube" />
+            </svg>
+        </a>
+    </li>
+    <?php endif; ?>
+
 </ul>
