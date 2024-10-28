9b1079ff4ec03c40137b841bf662ad2ebcc86b7e
Rich Holman <richholman@dogwonderPro.local>
2024-10-28 16:29:03 +0000

    Add new service worker

--- footer.php
+++ footer.php
@@ -31,13 +31,13 @@
 <script>
   if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function() {
-	  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-		// Successfully registered the Service Worker
-		console.log('Service Worker registration successful with scope: ', registration.scope);
-	  }).catch(function(err) {
-		// Failed to register the Service Worker
-		console.log('Service Worker registration failed: ', err);
-	  });
+	   navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
 	});
   }
 </script>
