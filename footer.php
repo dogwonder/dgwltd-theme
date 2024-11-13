b3f84836ea5946ed0ee31c2a185fc5e8521138b0
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2024-11-13 15:39:07 +0000

    Remove service worker

--- footer.php
+++ footer.php
@@ -28,18 +28,5 @@
       });
   }
 </script>
-<script>
-  if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function() {
-	   navigator.serviceWorker.register('/sw.js', { scope: '/' })
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((error) => {
-      console.error('Service Worker registration failed:', error);
-    });
-	});
-  }
-</script>
 </body>
 </html>
