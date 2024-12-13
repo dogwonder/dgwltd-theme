713b2ced118df2c49bfa28275cb38ed42357503a
Rich Holman <richholman@dogwonderPro.local>
2024-12-13 15:11:35 +0000

    Remove updater

--- functions.php
+++ functions.php
@@ -208,9 +208,3 @@ require_once get_template_directory() . '/inc/dgwltd-acf.php';
  * Functions for forms
  */
 require_once get_template_directory() . '/inc/dgwltd-forms.php';
-
-/**
- * Composer
- */
-// require_once get_template_directory() . '/vendor/autoload.php';
-// ( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run('https://wp.dgw.ltd/wp-json/git-updater/v1/themes-api/?slug=dgwltd');
